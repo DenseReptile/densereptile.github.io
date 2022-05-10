903e1044a5cd7261a502996970fc2352f3e129f2
Dense Reptile <80247368+DenseReptile@users.noreply.github.com>
2022-05-10 10:10:23 +0800

    Updated dependencies

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# wip
+# Rapide
+
+Yet another (untested) SvelteKit template.
+
+## Includes
+
+- ⚡ All of the benefits of SvelteKit — and more
+- 🖼️ Pre-configured UnoCSS
+  - 🎨 Tailwind CSS/Windi CSS style utility classes
+  - 🖌️ CSS icons
+  - 🔤 Custom fonts support
+- 🧪 Unit Testing with Vitest. E2E Testing with Playwright
+- 📄 Markdown Support
+- 👍 Typescript
+- 🔬 Opinionated ESLint configuration
+
+## Dependencies
+
+- [SvelteKit](https://kit.svelte.dev/)
+  - [Typescript](https://www.typescriptlang.org/)
+  - [Playwright](https://playwright.dev/)
+  - [ESLint](https://eslint.org/)
+    - [Deprecate](https://github.com/AlexMost/eslint-plugin-deprecate)
+    - [JSDoc](https://github.com/gajus/eslint-plugin-jsdoc)
+    - [Markdown](https://github.com/eslint/eslint-plugin-markdown)
+    - [No Unsanitized](https://github.com/mozilla/eslint-plugin-no-unsanitized)
+    - [pii](https://github.com/shiva-hack/eslint-plugin-pii)
+    - [SonarJS](https://github.com/SonarSource/eslint-plugin-sonarjs)
+  - [Prettier](https://prettier.io/)
+- [MDsveX](https://mdsvex.com/)
+- [Serve](https://github.com/vercel/serve)
+- [UnoCSS](https://github.com/unocss/unocss)
+  - Attributify Preset
+  - Icons Preset
+  - Typography Preset
+  - Web Fonts Preset
+  - Wind Preset
+- [Vitest](https://vitest.dev/)
+- [pnpm](https://pnpm.io/)
+
+## Checklist
+
+A list of things you should change prior to using this template.
+
+- [ ] Change the author name in `LICENSE` or change the `LICENSE` entirely.
+- [ ] Clear this `README.md` and replace it with your own.
+- [ ] Remove the placeholder files in `src`.
+- [ ] Customize to your liking.
+
+## See Also
+
+- [SvelteKit](https://kit.svelte.dev/)
+- [Vitesse](https://github.com/antfu/vitesse) for Vue users. Also served as inspiration
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import Unocss from "unocss/vite";
 import adapter from "@sveltejs/adapter-auto";
+import { extractorSvelte } from "@unocss/core";
 import { mdsvex } from "mdsvex";
 import preprocess from "svelte-preprocess";
-import { extractorSvelte } from "@unocss/core";
 import presetAttributify from "@unocss/preset-attributify";
 import presetIcons from "@unocss/preset-icons";
 import presetTypography from "@unocss/preset-typography";
 import presetWebFonts from "@unocss/preset-web-fonts";
 import presetWind from "@unocss/preset-wind";
+import transformerDirective from "@unocss/transformer-directives";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
 
-const dev = process.env.NODE_ENV === "development"; //
+const dev = process.env.NODE_ENV === "development"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,13 +24,13 @@ const config = {
 
   kit: {
     adapter: adapter(),
-    // ! Enable this for static site generation (GitHub Pages).
+    // * Enable this for static site generation (GitHub Pages).
     /*
      * "prerender": {
      *   "default": true
      * }
      */
-    // ! Enable this if deploying to GitHub Pages (unless your account's page, then don't)
+    // * Enable this if deploying to GitHub Pages (unless your account's page, then don't)
     /*
      * "paths": {
      *   "base": dev ? "" : "/your-repo-name"
@@ -40,13 +42,16 @@ const config = {
       plugins: [
         Unocss({
           extractors: [extractorSvelte],
+          transformers: [transformerDirective(), transformerVariantGroup()],
           presets: [
             presetWind(),
             presetIcons({ cdn: "https://esm.sh/" }),
             presetAttributify(),
             presetWebFonts({
+              // Currently UnoCSS only supports Google Fonts as a fonts provider.
               provider: "google",
               fonts: {
+                // The default fonts.
                 sans: ["Inter"],
                 mono: ["JetBrains Mono"],
                 serif: ["EB Garamond"],
@@ -60,6 +65,7 @@ const config = {
       ],
       resolve: {
         alias: {
+          // Path aliases
           $lib: "/src/lib",
           $assets: "/src/lib/assets",
           $audio: "/src/lib/assets/audio",
